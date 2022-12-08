258203faeee7044f1b483cfe7f87e588c744f9d6
Chris Barlas <barlas.3@buckeyelink.osu.edu>
2022-12-07 21:09:46 -0500

    Added documentation to structs.h

--- structs.h
+++ structs.h
@@ -1,5 +1,13 @@
+/*Chris Barlas and Ben Elleman*/
+
+/*To distinguish whether we are sorting cylinders less than POS or greater than POS*/
 enum SORT_TYPE { LOWER, HIGHER };
 
+/*Struct to hold the results of sort operation
+ * lower - holds the sorted array of cylinders less than POS
+ * higher - holds the sorted array of cylinders greater than POS
+ * balance - reports how many elements are in higher for use of combine function
+ */
 typedef struct 
 {
 	int *lower;
@@ -7,6 +15,11 @@ typedef struct
 	int balance;
 } SortResult;
 
+/*Struct to give every worker the data needed to start sorting the unsorted cylinders
+ * arr - pointer to the array of pointers to ints (the cylinders)
+ * type - whether we should do LOWER or HIGHER sort
+ * result - where the threads should put the resulting sorted array
+ */
 typedef struct
 {
 	int **arr;
